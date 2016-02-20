6304327c2df1988ca363681f31f022c80052f33c
Zurd2 <danielguerrero9@hotmail.com>
2016-02-20 17:12:11 +0100

    Revert "Error"
    This reverts commit 7500b021e5bf28436503def52e4ba0c56c9a3654.

--- RocketStats/index.php
+++ RocketStats/index.php
@@ -4,44 +4,48 @@ session_start ();
 
 $cargadorVistas = new CargadorVistas();
 
-$action = 1;
-
-if( isset($_GET["action"]) ){
-	$action = $_GET["action"];
+if (isset ( $_GET ["action"]) ){
+	
+	switch ($_GET ["action"]) {
+		
+		case 1:
+			//Establecer login
+			echo $cargadorVistas->validateForm();
+			break;
+		
+		case 2:
+			//Validar usuario del login
+			echo $cargadorVistas->validateUser();
+			break;
+		
+		case 3:
+			//Cerrar sesión activa
+			if(isset($_SESSION ["sessionUserName"] ))
+				echo $cargadorVistas->logout();
+			else
+				echo $cargadorVistas->main();
+			break;
+			
+		case 4:
+			//Cargar vista de jugadores
+			if(isset($_SESSION ["sessionUserName"] ))
+				echo $cargadorVistas->setJugadoresView();
+			else
+				echo $cargadorVistas->main();
+			break;
+			
+		case 5:
+			//Cargar vista de estadisticas
+			if(isset($_SESSION ["sessionUserName"] ))
+				echo $cargadorVistas->setEstadisticasView();
+			else
+				echo $cargadorVistas->main();
+			break;		
+		
+	}
+} 
+else {
+	echo $cargadorVistas->main();
 }
 
-switch( $action ){
-	case 1:
-		//Establecer login
-		echo $cargadorVistas->validateForm();
-		break;
-
-	case 2:
-		//Validar usuario del login
-		echo $cargadorVistas->validateUser();
-		break;
-
-	case 3:
-		//Cerrar sesión activa
-		if(isset($_SESSION ["sessionUserName"] ))
-			echo $cargadorVistas->logout();
-		else
-			echo $cargadorVistas->main();
-		break;
-
-	case 4:
-		//Cargar vista de jugadores
-		if(isset($_SESSION ["sessionUserName"] ))
-			echo $cargadorVistas->setJugadoresView();
-		else
-			echo $cargadorVistas->main();
-		break;
-
-	case 5:
-		//Cargar vista de estadisticas
-		if(isset($_SESSION ["sessionUserName"] ))
-			echo $cargadorVistas->setEstadisticasView();
-		else
-			echo $cargadorVistas->main();
-		break;
-}
+?>
